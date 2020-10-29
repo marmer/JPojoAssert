2394ae39491d78112ff5753643ce25da0aa1a4ca
MarMer <1055445+marmer@users.noreply.github.com>
2020-10-29 14:08:50 +0100

    The beginning of a vision for typesafe assertions

---
+++ JPojoAssert-Vision/src/test/kotlin/SomePojoTest.kt
@@ -0,0 +1,145 @@
+import org.hamcrest.Matcher
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
+import org.junit.jupiter.api.Test
+
+data class SomePojo(
+    val firstProperty: String,
+    val secondProperty: Int
+)
+
+
+internal class SomePojoTest {
+    @Test
+    fun `just some test`() {
+        // Preparation
+
+        val pojo = SomePojo("Some value", 42)
+
+        // Execution
+
+        // Assertion
+        SomePojoAsserter.assertThat(pojo)
+            .matches(hasProperty("notExistingProperty"))
+            .isInstanceOfSomePojo()
+            .withFirstProperty()
+            .withFirstProperty("x")
+            .withFirstProperty(equalTo("x"))
+            .withFirstProperty { assertThat(it, equalTo("x")) }
+            .withSecondProperty()
+            .withSecondProperty(3)
+            .withSecondProperty(equalTo(3))
+            .withSecondProperty { assertThat(it, equalTo("3")) }
+//            .assertHardly()
+            .assertSoftly()
+    }
+}
+
+
+class SomePojoAsserter private constructor(
+    val pojo: SomePojo,
+    val additionalMessage: String = "SomePojo",
+    val assertions: List<() -> Unit> = emptyList()
+) {
+
+    fun assertHardly() {
+        assertions.forEach { it() }
+    }
+
+    private fun (() -> Unit).toAssertionError(): AssertionError? =
+        try {
+            this()
+            null
+        } catch (e: AssertionError) {
+            e
+        }
+
+    fun assertSoftly() =
+        with(assertions
+            .map { it.toAssertionError() }
+            .filterNotNull()
+        ) {
+            if (isNotEmpty()) throw AssertionError(map {
+                //to readable assertion
+
+                it.message + "\n" + it.stackTrace.map { "\t$it" }.joinToString("\n")
+            }.joinToString(separator = "\n\n"))
+        }
+
+    fun matches(matcher: Matcher<SomePojo>) =
+        plusAssertion { assertThat(pojo, matcher) }
+
+    // ###### First Property ###########
+    fun withFirstProperty() =
+        plusAssertion {
+            assertThat(
+                additionalMessage,
+                pojo,
+                hasProperty("firstProperty")
+            )
+        }
+
+    fun withFirstProperty(value: String?) =
+        plusAssertion {
+            assertThat(
+                additionalMessage,
+                pojo,
+                hasProperty("firstProperty", equalTo(value))
+            )
+        }
+
+    fun withFirstProperty(matcher: Matcher<String>) =
+        plusAssertion {
+            assertThat(
+                additionalMessage,
+                pojo,
+                hasProperty("firstProperty", matcher)
+            )
+        }
+
+    fun withFirstProperty(dynamicAssertion: (String) -> Unit) =
+        plusAssertion { dynamicAssertion(pojo.firstProperty) }
+
+    // ###### Second Property ###########
+    fun withSecondProperty() =
+        plusAssertion {
+            assertThat(
+                additionalMessage,
+                pojo,
+                hasProperty("secondProperty")
+            )
+        }
+
+    fun withSecondProperty(value: Int?) =
+        plusAssertion {
+            assertThat(
+                additionalMessage,
+                pojo,
+                hasProperty("secondProperty", equalTo(value))
+            )
+        }
+
+    fun withSecondProperty(matcher: Matcher<Int>) =
+        plusAssertion {
+            assertThat(
+                additionalMessage,
+                pojo,
+                hasProperty("secondProperty", matcher)
+            )
+        }
+
+    fun withSecondProperty(dynamicAssertion: (Int) -> Unit) =
+        plusAssertion { dynamicAssertion(pojo.secondProperty) }
+
+
+    private fun plusAssertion(newAssertion: () -> Unit) =
+        SomePojoAsserter(pojo, additionalMessage, assertions + newAssertion)
+
+    fun isInstanceOfSomePojo() = plusAssertion { assertThat(pojo, instanceOf(SomePojo::class.java)) }
+
+    companion object {
+        @JvmStatic
+        fun assertThat(pojo: SomePojo) = SomePojoAsserter(pojo)
+    }
+}
+
