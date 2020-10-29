a3925c9ba7f153e9326fdce9f1985b1a4b6947f4
MarMer <1055445+marmer@users.noreply.github.com>
2020-10-29 21:25:41 +0100

    sonar reporting

---
+++ JPojoAssert-processor/src/test/java/SomeClassKtTest.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test
+
+internal class SomeClassKtTest {
+    @Test
+    fun `throw me away`() {
+        // Preparation
+
+        // Execution
+        val result = someMethod()
+
+        // Assertion
+    }
+}
