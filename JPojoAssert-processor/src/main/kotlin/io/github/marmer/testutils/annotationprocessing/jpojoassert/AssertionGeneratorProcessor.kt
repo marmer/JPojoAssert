5111321596a15638cf52b9961585e6fc08cb47fb
MarMer <1055445+marmer@users.noreply.github.com>
2020-11-01 22:37:34 +0100

    refactoring

--- JPojoAssert-processor/src/main/kotlin/io/github/marmer/testutils/annotationprocessing/jpojoassert/AssertionGeneratorProcessor.kt
+++ JPojoAssert-processor/src/main/kotlin/io/github/marmer/testutils/annotationprocessing/jpojoassert/AssertionGeneratorProcessor.kt
@@ -1,13 +1,8 @@
 package io.github.marmer.testutils.annotationprocessing.jpojoassert
 
 import com.google.auto.service.AutoService
-import com.squareup.javapoet.*
-import com.squareup.javapoet.TypeSpec.classBuilder
-import java.time.LocalDate
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
-import javax.lang.model.element.Modifier
-import javax.lang.model.element.PackageElement
 import javax.lang.model.element.TypeElement
 
 
@@ -20,57 +15,19 @@ class AssertionGeneratorProcessor : AbstractProcessor() {
     override fun process(set: Set<TypeElement>, roundEnvironment: RoundEnvironment): Boolean {
         if (!roundEnvironment.processingOver() && set.containsTypeInfoFor(GenerateAsserter::class.java)) {
             roundEnvironment.getElementsAnnotatedWith(GenerateAsserter::class.java)
-                .forEach { generate(it.getAnnotation(GenerateAsserter::class.java)) }
+                .forEach {
+                    val baseType =
+                        processingEnv.elementUtils.getTypeElement(it.getAnnotation(GenerateAsserter::class.java).value)
+
+                    PojoAsserterGenerator(processingEnv, baseType).generate()
+                }
             return true
         }
         return false
     }
 
-    private fun generate(configuration: GenerateAsserter) {
-        val baseType = processingEnv.elementUtils.getTypeElement(configuration.value)
-
-        generate(baseType)
-    }
-
-    private fun generate(baseType: TypeElement) = JavaFile.builder(
-        baseType.packageElement.toString(),
-        classBuilder("${baseType.simpleName}Asserter")
-            .addModifiers(Modifier.PUBLIC)
-            .addAnnotation(generatedAnnotation())
-            .addField(pojoAssertionBuilderField(baseType))
-            .build()
-    ).build()
-        .writeTo(processingEnv.filer)
-
-    private fun pojoAssertionBuilderField(baseType: TypeElement): FieldSpec = FieldSpec.builder(
-        getBuilderFieldTypeFor(baseType),
-        "pojoAssertionBuilder",
-        Modifier.PRIVATE,
-        Modifier.FINAL
-    ).build()
-
-    private fun getBuilderFieldTypeFor(baseType: TypeElement): ParameterizedTypeName? {
-        return ParameterizedTypeName.get(
-            ClassName.get(PojoAssertionBuilder::class.java),
-            TypeName.get(baseType.asType())
-        )
-    }
-
-    private fun generatedAnnotation(): AnnotationSpec? {
-        return AnnotationSpec.builder(Generated::class.java)
-            .addMember("value", "\$S", javaClass.name)
-            .addMember("date", "\$S", LocalDate.now())
-            .build()
-    }
-
-
-    private val TypeElement.packageElement: PackageElement
-        get() = processingEnv.elementUtils.getPackageOf(this)
 
 }
-
-private fun String.toPackageName(): String =
-    removeRange(lastIndexOf("."), length)
 
 private fun <T> Iterable<TypeElement>.containsTypeInfoFor(type: Class<T>): Boolean =
     any { it.qualifiedName.toString() == type.name }
