47096245036601acb1783f2e9cba8a25df8925b3
MarMer <1055445+marmer@users.noreply.github.com>
2020-11-05 22:13:27 +0100

    there is currently no need for separate kotlin interfaces

--- JPojoAssert-core/src/main/kotlin/io/github/marmer/testutils/annotationprocessing/jpojoassert/PojoAssertionBuilder.kt
+++ JPojoAssert-core/src/main/kotlin/io/github/marmer/testutils/annotationprocessing/jpojoassert/PojoAssertionBuilder.kt
@@ -13,8 +13,6 @@ class PojoAssertionBuilder<T>(
     fun add(assertionCallback: AssertionCallback<T>) =
         PojoAssertionBuilder(pojo, assertionCallbacks + { assertionCallback.accept(pojo) }, "What a good day to throw")
 
-    fun add(assertionCallback: (T) -> Unit) = add(AssertionCallback { assertionCallback(it) })
-
     fun assertHardly() =
         assertionCallbacks.forEach { callback ->
             callback.toThrownExceptionOrNull()
