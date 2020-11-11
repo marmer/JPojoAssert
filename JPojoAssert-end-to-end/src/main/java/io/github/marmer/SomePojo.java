6219f4f3bb12a8ec53faa8b61ad628b69dcd0d12
MarMer <1055445+marmer@users.noreply.github.com>
2020-11-11 12:38:27 +0100

    - end to end test extended for the handling of generics

--- JPojoAssert-end-to-end/src/main/java/io/github/marmer/SomePojo.java
+++ JPojoAssert-end-to-end/src/main/java/io/github/marmer/SomePojo.java
@@ -3,8 +3,11 @@ package io.github.marmer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 @AllArgsConstructor
-public class SomePojo {
+public abstract class SomePojo<T extends CharSequence> {
     private final String firstName;
+    private final List<T> titles;
 }
--- JPojoAssert-end-to-end/src/test/java/io/github/marmer/SomePojoTest.java
+++ JPojoAssert-end-to-end/src/test/java/io/github/marmer/SomePojoTest.java
@@ -3,7 +3,10 @@ package io.github.marmer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,12 +19,14 @@ class SomePojoTest {
 
         final var assertionError = assertThrows(AssertionError.class,
                 // Execution
-                () -> SomePojoAsserter.prepareFor(new SomePojo("Helge"))
+                () -> SomePojoAsserter.prepareFor(new SomePojo<>("Helge", List.of("Prof.", "Dr.")) {
+                })
                         .with(it -> org.junit.jupiter.api.Assertions.assertEquals("HelgeX", it.getFirstName(), "firstName"))
                         .with(it -> {
                             throw new Exception("Fancy Exception");
                         })
                         .withFirstName(it -> org.junit.jupiter.api.Assertions.assertEquals("HelgeY", it))
+                        .withTitles(it -> assertThat(it, contains("Prof.", "Dr.")))
                         .assertAll());
         // Assertion
         assertAll(
